de98830592a0423d371b0485e33f1e5e47b506e8
Vinicius Fonseca <viniciusfonseca.alcans@gmail.com>
2025-10-25 16:12:21 -0300

    criando organizando as rotas e criando o primeiro controller

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client/";
+import { appRoutes } from "./http/routes.js";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
-
-prisma.user.create({
-  data: {
-    name: "Vinicius Fonseca",
-    email: "viniciusfonseca553@gmail.com",
-  },
-});
+app.register(appRoutes);
---
+++ src/http/routes.ts
@@ -0,0 +1,6 @@
+import type { FastifyInstance } from "fastify";
+import { register } from "./controllers/register.controller.js";
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post("/users", register);
+}
